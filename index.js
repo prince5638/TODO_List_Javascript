a9ae1d6a0fc4f54f03274efa6bf1ae076fb9cfb0
Prince Changani <princechangani56385638@gmail.com>
2023-08-29 01:03:47 +0530

    reload item list function is added to see all the list's added

--- index.js
+++ index.js
@@ -1,27 +1,57 @@
 // Adding list in TODO's
-function getAndUpdate()
-{
-  console.log("One item added in TODO's list...");
-  
-  tit = document.getElementById("title").value;
-  desc = document.getElementById("description").value;
-  
-  if(localStorage.getItem('itemsJson') == null)
-  {
-    itemJsonArray = [];
-    itemJsonArray.push([tit, desc]);
-    
-    localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray))
-  }
-  else{
-    itemJsonArrayStr = localStorage.getItem('itemsJson');
-    itemJsonArray = JSON.parse(itemJsonArrayStr);
-    itemJsonArray.push([tit, desc]);
-    
-    localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray));
-  }
-};
+    function getAndUpdate()
+    {
+      console.log("One item added in TODO's list...");
+      
+      tit = document.getElementById("title").value;
+      desc = document.getElementById("description").value;
+      
+      if(localStorage.getItem('itemsJson') == null)
+      {
+        itemJsonArray = [];
+        itemJsonArray.push([tit, desc]);
+        
+        localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray))
+      }
+      else{
+        itemJsonArrayStr = localStorage.getItem('itemsJson');
+        itemJsonArray = JSON.parse(itemJsonArrayStr);
+        itemJsonArray.push([tit, desc]);
+        
+        localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray));
+      }
+      reloadItemList();
+    };
 
-add = document.getElementById("add");
+    // featching all the list before and after every action takes place. 
+    function reloadItemList()
+    {
+      if(localStorage.getItem('itemsJson') == null)
+      {
+        console.log("Your TODO list is empty!");
+      }
+      else{
+        // Populating the table
+        let tableBody = document.getElementById("tableBody")
+        let str = "";
+  
+        itemJsonArrayStr = localStorage.getItem('itemsJson');
+        itemJsonArray = JSON.parse(itemJsonArrayStr);
+  
+        itemJsonArray.forEach((element, index) => {
+          str += `
+            <tr>
+              <th scope="row">${index + 1}</th>
+              <td>${element[0]}</td>
+              <td>${element[1]}</td>
+              <td><button class="btn btn-primary btn-sm" onclick="deleted(${index})">Delete</button></td>
+            </tr>`
+        });
+        tableBody.innerHTML = str;
+      }
+    }
 
-add.addEventListener("click", getAndUpdate);
+    add = document.getElementById("add");
+
+    add.addEventListener("click", getAndUpdate); 
+    reloadItemList();
